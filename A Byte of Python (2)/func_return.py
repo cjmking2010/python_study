17e4726b2066b0058f5b502d4b45b462bc6ffb4c
cjm20008@126.com <cjm20008@126.com>
2018-01-08 17:34:04 +0800

    简明Python教程的第2次练习

---
+++ A Byte of Python (2)/func_return.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: func_return.py
+# Authoer: peter.chen
+
+
+def maximum(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+
+print maximum(2,3)
